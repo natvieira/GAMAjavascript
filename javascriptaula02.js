3fdbc4dd845bf2a8762a0b829debdc3f10a4e57a
natalia vieira <verkappte@hotmail.com>
2023-02-22 16:09:29 -0300

    upando exercícios aula 1 e 2

---
+++ javascriptaula02.js
@@ -0,0 +1,31 @@
+let ingred1 = "farinha";
+let ingred2 = "leite";
+let ingred3 = "fermento";
+let ingred4 = "sal";
+
+console.log('INGREDIENTES NECESSÁRIOS');
+console.log(ingred1);
+console.log(ingred2);
+console.log(ingred3);
+console.log(ingred4);
+
+let misturar = ingred1 + "-" + ingred2 + "-" + ingred3 + "-" + ingred4;
+
+console.log('MISTURAR');
+console.log(misturar);  
+
+let forma;
+forma = "untar com óleo e farinha";
+forma += " e adicionar os ingredientes anteriores: " + misturar;
+
+console.log('PARA LEVAR AO FORNO');
+console.log(forma);    
+
+let cobertura1 = "creme";
+let cobertura2 = "granulado";
+let cobertura3 = "morangos picados";
+let cobertura = cobertura1 + "-" + cobertura2 + "-" +cobertura3;
+
+console.log ('PARA CONFEITAR');
+console.log ('Utilize ' + cobertura);
+
