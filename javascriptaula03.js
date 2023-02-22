8b7763eaf92a1f6efcfe5a9f5d556dd71ebb86f6
natalia vieira <verkappte@hotmail.com>
2023-02-22 16:51:50 -0300

    inclusão do arquivo da aula 3

---
+++ javascriptaula03.js
@@ -0,0 +1,29 @@
+//exemplos de tipos de dados:
+
+let variavelString = "texto";
+let variavelnumeroInteiro = 10;
+let variavelFloat = 1.25;
+let variavelBooleana1 = true;
+let variavelBooleana2 = false;
+
+//uso da função typeof
+
+console.log("variavelString = " + typeof(variavelString));
+console.log("variavelnumeroInteiro = " + typeof(variavelnumeroInteiro));
+console.log("variavelFloat = " + typeof(variavelFloat));
+console.log("variavelBooleana1 = " + typeof(variavelBooleana1));
+console.log("variavelBooleana2 = " + typeof(variavelBooleana2));
+
+//uso de array
+
+let variavelArray = [];
+
+variavelArray [0] = variavelString;
+variavelArray.push = variavelnumeroInteiro;
+variavelArray.push = variavelFloat;
+variavelArray.push = variavelBooleana1;
+variavelArray.push = variavelBooleana2;
+
+console.log("variavelArray = " + typeof(variavelArray));
+
+//minuto 4 da aula 04
